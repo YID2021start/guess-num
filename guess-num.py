86cad9d887c5579ddbe105b000a47aece561858e
YID <yi19871230@hotmail.com>
2021-06-19 21:39:38 +0800

    New add file

--- guess-num.py
+++ guess-num.py
@@ -1,6 +1,10 @@
 import random
+start = input('請決定隨機數字範圍開始值:')
+end = input('請決定隨機數字範圍結束值:')
+start = int(start)
+end = int(end)
+r = random.randint(start,end)
 count = 0
-r = random.randint(1,10)
 while True:
 	count += 1 
 	num = input('請猜數字:')
