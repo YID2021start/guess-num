a4203a6a2b042f0daf866cf3f4073b90d30c77a1
YID <yi19871230@hotmail.com>
2021-06-19 21:31:31 +0800

    Guess add file

--- guess-num.py
+++ guess-num.py
@@ -1,12 +1,16 @@
 import random
+count = 0
 r = random.randint(1,10)
 while True:
+	count += 1 
 	num = input('請猜數字:')
 	num = int(num)
 	if num == r:
 		print('你猜對了!')
+		print('你已經猜了', count,'次')
 		break
 	elif num > r:
 		print('比答案大')	
 	elif num < r:
-		print('比答案小')	
+		print('比答案小')
+	print('你已經猜了', count,'次')
